a3260c1a4b9ff72a72eaa0c894a4993158e2273e
anturin <anturin@qq.com>
2025-03-06 12:26:55 +0800

    千问模型

---
+++ Turin.OpenAI/Models/QwenAiModel.cs
@@ -0,0 +1,17 @@
+using Newtonsoft.Json;
+
+namespace Turin.OpenAI.Models;
+
+[JsonConverter(typeof(OpenAiEnumConverter))]
+public enum QwenAiModel
+{
+    QwenPlus,
+    QwenPlusLatest,
+    QwenTurbo,
+    QwenTurboLatest,
+    QwenLong,
+    QwenLongLatest,
+    QwenMax,
+    QwenMaxLatest,
+    QwenCoderPlus,
+}
